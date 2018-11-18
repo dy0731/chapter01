87b942ba558adf9c4c1f3728fff34d0ebd70a0be
nexte <nexte@LAPTOP-IB7CQKHH>
2018-11-18 21:30:45 +0900

    Gugudan Pretty

--- src/chapter01/GugudanPretty.java
+++ src/chapter01/GugudanPretty.java
@@ -3,7 +3,14 @@ package chapter01;
 public class GugudanPretty {
 
 	public static void main(String[] args) {
-		
+		for (int i = 1; i <= 9; i++) {
+
+			for (int j = 1; j <= 9; j++) {
+				System.out.print(j + " * " + i + " = " + (i * j) + "\t");
+			}
+			
+			System.out.println("");
+		}
 
 	}
 
